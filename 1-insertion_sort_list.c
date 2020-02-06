900e6a04fe567fd7bb2b265126943850887557e1
Anthony <huggins9000211@gmail.com>
2020-02-06 10:47:31 -0500

    test

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -10,14 +10,5 @@ void insertion_sort_list(listint_t **list)
 	listint_t *temp = NULL; 
 	listint_t *current = *list;
 	
-	while (current)
-	{
-		if (current->n < current->prev->n)
-		{
-			/* code */
-		}
-		
-	}
-	
 }
 
